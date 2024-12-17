dd19629e6126f7bbc2c91a759a05439d54f1672f
arvind <ak5943436@gmail.com>
2024-12-17 14:21:39 +0530

    complete calculator

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function backspace(){
     document.getElementById("answer").value = str
 }
 
+document.addEventListener('keydown', function(e) {
+    if(["(",")","*","+","-","/","%",".","Backspace","Enter","1","2","3","4","5","6","7","8","9","0"].includes(e.key)){
+        if(e.key === "Backspace"){
+            backspace()
+        }
+        else if(e.key === "Enter"){
+            e.preventDefault()
+            equalto()
+        }
+        else{
+            str = str + e.key
+            document.getElementById("answer").value = str
+        }
+    }
+});
